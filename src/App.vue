b4619bea8b063bd386921e5dd944ace12436439d
mmd1380 <64871762+mmd1380@users.noreply.github.com>
2020-10-24 11:06:19 +0330

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template id="app">
+  <v-app>
+    <v-container>
+      <AppForm/>
+    </v-container>
+  </v-app>
+</template>
+
+
+<style>
+@font-face {
+  font-family: 'b nazanin';
+  src: url('http://cdn.bayan.ir/blog/templates/shared/fonts/BNazanin.eot?view#iefix') format('embedded-opentype'),
+  url('http://cdn.bayan.ir/blog/templates/shared/fonts/BNazanin.woff?view') format('woff'),
+  url('http://cdn.bayan.ir/blog/templates/shared/fonts/BNazanin.ttf?view') format('truetype');
+}
+
+#app {
+  font-family: 'b nazanin';
+}
+
+
+</style>
+<script>
+import AppForm from "@/components/AppForm";
+export default {
+  components: {AppForm}
+}
+</script>
